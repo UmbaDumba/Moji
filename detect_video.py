a9d5394a14f04bc4910da2b5d15ae94d31122568
young0831 <bnjj123@naver.com>
2025-06-02 11:38:50 +0900

    add

--- detect_video.py
+++ detect_video.py
@@ -1,43 +1,55 @@
 from ultralytics import YOLO
 import cv2
+from deepface import DeepFace
 
-# 1단계: 사람 탐지용 YOLO 모델 (사전 학습된 COCO)
-person_detector = YOLO("yolov8n.pt")  # 또는 yolov8s.pt 등
-
-# 2단계: 학습된 눈/눈동자 모델
+# YOLO 모델들
+person_detector = YOLO("yolov8s.pt")
 eye_model = YOLO("runs/eye_yolo_train/weights/best.pt")
 
-# 웹캠 열기
-cap = cv2.VideoCapture(1)  # 1 또는 0은 캠 번호
+# 홍길동 벡터 준비
+hong_embedding = DeepFace.represent(img_path=r"C:\Users\user\Moji\test\images\u7_jpg.rf.e313c4ab387ad6ee0ca70414619e6c9c.jpg", model_name="Facenet")[0]["embedding"]
+
+# 웹캠
+cap = cv2.VideoCapture(2)
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # 1단계: 사람 검출
+    # 1단계: 사람 탐지
     person_results = person_detector(frame)
     person_boxes = person_results[0].boxes
 
     for box in person_boxes:
         cls_id = int(box.cls[0].item())
-        if cls_id != 0:  # 0: person 클래스 (YOLO COCO 기준)
+        if cls_id != 0:  # person 클래스
             continue
 
-        # 박스 좌표 추출
+        # 사람 crop
         x1, y1, x2, y2 = map(int, box.xyxy[0])
         person_crop = frame[y1:y2, x1:x2]
+        try:
+            # 2단계: 얼굴 유사도 비교
+            result = DeepFace.verify(person_crop, "./woo_dataset/test/images/u7_jpg.rf.e313c4ab387ad6ee0ca70414619e6c9c.jpg", model_name="Facenet", enforce_detection=False)
 
-        # 2단계: 눈/눈동자 모델로 감지
-        eye_results = eye_model(person_crop)
-        eye_annotated = eye_results[0].plot()
+            if result["verified"] and result["distance"] < 0.4:  # threshold 조정 가능
+                # WOO라면 눈 탐지
+                eye_results = eye_model(person_crop)
+                eye_annotated = eye_results[0].plot()
 
-        # 원본 프레임에 결과 붙여넣기
-        frame[y1:y2, x1:x2] = eye_annotated
+                # 결과 덮어쓰기
+                frame[y1:y2, x1:x2] = eye_annotated
 
-    # 결과 출력
-    cv2.imshow("YOLOv8 Person + Eye Detection", frame)
+                # 이름 표시
+                cv2.putText(frame, "WOO", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
 
+        except Exception as e:
+            print("Error Recognition:", e)
+            continue
+
+    # 출력
+    cv2.imshow("WOO trakcing + eye detecting", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
